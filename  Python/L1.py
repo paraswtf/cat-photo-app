9b6f45049c6b1748602d0efc3d46f295794767a1
Abel-14 <abelroy007@gmail.com>
2023-07-16 23:09:12 +0530

    .

---
+++  Python/L1.py
@@ -0,0 +1,26 @@
+#TODO
+
+
+message = "Keep Smiling"
+userno = 101
+
+float = 9.8*10-2
+
+print(f"\nfloat value : {float} \ndata type : {type(float)} \nid : {id(float)}")
+
+
+
+boolean = True
+print(f"\nboolean value : {boolean} \ndata type : {type(boolean)} \nid : {id(boolean)}\n\n")
+
+s1 = 'Hello And Welcome To This Grand Occassion'
+print(type(s1))
+print(f'{s1[0]}\n{s1[1]}\n{s1[2]}\n{s1[3]}\n{s1[4]}\n{len(s1)}\n')
+
+l1 = ['Hello','And','Welcome','To','This','Grand','Occassion']
+print(type(l1))
+print(f'{l1[0]}\n{l1[1]}\n{l1[2]}\n{l1[3]}\n{l1[4]}\n{len(l1)}\n')
+
+t1 = ('Hello','And','Welcome','To','This','Grand','Occassion')
+print(type(t1))
+print(f'{t1[0]}\n{t1[1]}\n{t1[2]}\n{t1[3]}\n{t1[4]}\n{len(t1)}\n')
